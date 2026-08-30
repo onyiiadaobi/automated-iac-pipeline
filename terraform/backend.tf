4a0a67729cd916c9385d4ab8353f06a2700b1039
Onyiiadaobi <onyiiadaobi@gmail.com>
2026-08-30 10:30:28 +0100

    Update Terraform S3 state locking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "onyinye-statefile"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
-    dynamodb_table = "onyinye-statelock"
+    bucket       = "onyinye-statefile"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
